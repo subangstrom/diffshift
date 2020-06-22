4b37a59431b55f5505e218669075707cb942ae57
xichen <xichen19@mit.edu>
2020-06-22 01:56:52 -0400

    update diffalign to use vector1, vector2

--- alignment/diffalign.py
+++ alignment/diffalign.py
@@ -3,46 +3,46 @@ import numpy as np
 import scipy
 import scipy.optimize
 from matplotlib import pylab as plt
-from scipy import signal
 from skimage import io
 from skimage.transform import AffineTransform, warp
-from skimage.transform import rotate
+
 
 
 class diffalign:
 
-    def __init__(self,filepath,deterctorsize):
+    def __init__(self, filepath):
 
         images = io.imread(filepath)
         self.images = np.array([p for p in images[::-1]])
         self.size = self.images.shape
         self.num_patterns = int(np.sqrt(self.size[0]))
-        self.deterctorsize = deterctorsize
+        self.deterctorsize = self.size[1]
 
-    def select_images(self,indexs):
-        alignImages = np.zeros([len(indexs),self.size[1],self.size[2]])
+    def select_images(self, indexs):
+        # this function is used to select a set of sub-patterns for alignment:
 
-        for num,index in enumerate(indexs,start = 0):
-            alignImages[num,:,:] = self.images[int(index),:,:]
+        alignImages = np.zeros([len(indexs), self.size[1], self.size[2]])
 
-        # self.alignImages = alignImages
+        for num, index in enumerate(indexs, start=0):
+            alignImages[num, :, :] = self.images[int(index), :, :]
 
         return alignImages
 
+    def expandImages(self, images, width, overlap=0.1):
+        # this function zeropad all the sub-patterns to the size of the final image with a extra width at the edge
+        # width: is the extra width at the edge of the final image
+        # overlap: an esitimation of the overlap percent between sub-patterns
 
-    def expandImages(self,images,width):
-
-        Size = images.shape
-        num_patterns = int(np.sqrt(Size[0]))
-        size = 2*width+num_patterns*Size[1]
-        expanded_images = np.zeros([Size[0],size,size])
+        size = images.shape
+        num_patterns = int(np.sqrt(size[0]))
+        finalsize = 2 * width + int(num_patterns * size[1] * (1-overlap) + size[1] * overlap)
+        expanded_images = np.zeros([size[0], finalsize, finalsize])
         counter = 0
 
         for image in images:
-            holdingPattern = np.zeros([size,size])
-            row = int(width + (num_patterns-1-counter%num_patterns)*self.size[1])
-            column = int(width + int(counter/num_patterns)*Size[1])
-            holdingPattern[row:row+Size[1],column:column+Size[1]] = image
+            holdingPattern = np.zeros([finalsize, finalsize])
+
+            holdingPattern[width:width + size[1], width:width + size[1]] = image
             expanded_images[counter] = holdingPattern
             counter += 1
 
@@ -50,8 +50,9 @@ class diffalign:
 
     def displaypatterns(self):
 
-        expandedImages = self.expandImages(self.images,10)
-        combined = self.subpixelstitch(expandedImages,0,0,0,0,0)
+        expandedImages = self.expandImages(self.images, self.num_patterns+5, overlap=0)
+        v = self.deterctorsize + 1
+        combined = self.subpixelstitch(expandedImages, [v, 0], [0, v])
 
         plt.figure()
         plt.imshow(np.log(combined))
@@ -62,181 +63,96 @@ class diffalign:
 
         counter = 0
 
-        for i in range(1,self.num_patterns+1):
-            for j in range(1,self.num_patterns+1):
+        for i in range(1, self.num_patterns + 1):
+            for j in range(1, self.num_patterns + 1):
+                x = (i - 1) * self.size[1] + self.size[1] / 2
+                y = (self.num_patterns - j) * self.size[2] + self.size[2] / 2
 
-                x = (i-1)*self.size[1] + self.size[1]/2
-                y = (self.num_patterns-j)*self.size[2] + self.size[2]/2
-
-                plt.text(x,y,str(counter))
+                plt.text(x, y, str(counter))
                 counter = counter + 1
 
-    def pixel_align(self,index):
+    def subpixelshift(self, image, vector):
 
-        center = np.log(self.images[index].astype(np.float64))
-        top = np.log(self.images[index+1].astype(np.float64))
-        left = np.log(self.images[index-self.num_patterns,:,:].astype(np.float64))
-
-        corr = signal.correlate2d(top, center, boundary='symm', mode='same')
-        y, x = np.unravel_index(np.argmax(corr), corr.shape)
-
-        overlapy0 = self.size[1] - 1 - y + self.size[1]/2
-        shiftx0 = self.size[1]/2 - x
-
-        corr = signal.correlate2d(left, center, boundary='symm', mode='same')
-        y, x = np.unravel_index(np.argmax(corr), corr.shape)
-
-        overlapx0 = self.size[1] - 1 - x + self.size[1]/2
-        shifty0 = self.size[1]/2 - y
-
-        return overlapx0,overlapy0,shiftx0,shifty0
-
-    def subpixelshift_rotate(self,image,vector,rotation):
-
-        transform = AffineTransform(translation=vector)
-        shifted = warp(image, transform, mode='wrap',order = 1, preserve_range=True)
+        transform = AffineTransform(translation=[-vector[0],-vector[1]])
+        shifted = warp(image, transform, mode='wrap', order=1, preserve_range=True)
         shifted = shifted.astype(image.dtype)
-        shifted_rotated = rotate(shifted,rotation,order=1,mode='constant', cval=0.0)
 
-        ones = shifted_rotated > 0
+        ones = shifted > 0
         ones = ones.astype('uint8')
-        kernel = np.ones((5,5),np.uint8)
-        erosion = cv2.erode(ones,kernel,iterations = 1)
+        kernel = np.ones((5, 5), np.uint8)
+        erosion = cv2.erode(ones, kernel, iterations=1)
 
-        return_image = shifted_rotated*erosion
+        return_image = shifted * erosion
 
         return return_image
 
-    def caldifference_X_sub(self,image1,image2,range_x,range_y):
+    def caldifference_X_sub(self, image1, image2, range_x, range_y):
         imsize = image1.shape[0]
-        residual = (image1[imsize-range_x:imsize,range_y:imsize-range_y]-image2[imsize-range_x:imsize,range_y:imsize-range_y])/image1[imsize-range_x:imsize,range_y:imsize-range_y]*100
+        residual = (image1[imsize - range_x:imsize, range_y:imsize - range_y] - image2[imsize - range_x:imsize, range_y:imsize - range_y]) / image1[imsize - range_x:imsize, range_y:imsize - range_y] * 100
         return residual
 
-    def caldifference_Y_sub(self,image1,image2,range_x,range_y):
+    def caldifference_Y_sub(self, image1, image2, range_x, range_y):
         imsize = image1.shape[0]
-        residual = (image1[range_x:imsize-range_x,imsize-range_y:]-image2[range_x:imsize-range_x,imsize-range_y:])/image1[range_x:imsize-range_x,imsize-range_y:]*100
+        residual = (image1[range_x:imsize - range_x, imsize - range_y:] - image2[range_x:imsize - range_x, imsize - range_y:]) / image1[range_x:imsize - range_x, imsize - range_y:] * 100
         return residual
 
-    def fit_overlapxy_shiftxy(self,index,overlapx0,overlapy0,shiftx0,shifty0,range_x,range_y):
+    def fit_v1_v2(self, index, v1_0, v2_0, range_x, range_y):
 
         center = self.images[index].astype(np.float64)
 
-        top = self.images[index+1].astype(np.float64)
-        bottom = self.images[index-1].astype(np.float64)
+        top = self.images[index + 1].astype(np.float64)
+        bottom = self.images[index - 1].astype(np.float64)
 
-        right = self.images[index+self.num_patterns,:,:].astype(np.float64)
-        left = self.images[index-self.num_patterns,:,:].astype(np.float64)
+        right = self.images[index + self.num_patterns, :, :].astype(np.float64)
+        left = self.images[index - self.num_patterns, :, :].astype(np.float64)
 
-        def errfun(overlapxy_shiftxy):
+        def errfun(x):
 
-            vector = [overlapxy_shiftxy[2],overlapxy_shiftxy[1]]
-            # vector = [0,overlapxy_shiftxy[1]]
+            vector1 = [x[0], x[1]]
 
-            center_shiftedrotated = self.subpixelshift_rotate(center,vector,0)
-            top_rotated = self.subpixelshift_rotate(top,[0,0],0)
-            bottom_shiftedrotated = self.subpixelshift_rotate(bottom,vector,0)
-            center_rotated = self.subpixelshift_rotate(center,[0,0],0)
+            center_shifted = self.subpixelshift(center, vector1)
+            right_shifted = self.subpixelshift(right, vector1)
 
-            residual_top = self.caldifference_X_sub(center_shiftedrotated,top_rotated,range_x,range_y)
-            residual_bottom = self.caldifference_X_sub(bottom_shiftedrotated,center_rotated,range_x,range_y)
+            residual_left = self.caldifference_Y_sub(center_shifted, left, range_y, range_x)
+            residual_right = self.caldifference_Y_sub(right_shifted, center, range_y, range_x)
 
-            residualx = np.append(np.ravel(residual_top),np.ravel(residual_bottom))
+            residualy = np.append(np.ravel(residual_left), np.ravel(residual_right))
 
-            vector = [overlapxy_shiftxy[0],overlapxy_shiftxy[3]]
-            # vector = [overlapxy_shiftxy[0],0]
+            vector2 = [x[2], x[3]]
 
-            center_shiftedrotated = self.subpixelshift_rotate(center,vector,0)
-            left_rotated = self.subpixelshift_rotate(left,[0,0],0)
-            right_shiftedrotated = self.subpixelshift_rotate(right,vector,0)
-            center_rotated = self.subpixelshift_rotate(center,[0,0],0)
+            center_shifted = self.subpixelshift(center, vector2)
+            bottom_shifted = self.subpixelshift(bottom, vector2)
 
-            residual_left = self.caldifference_Y_sub(center_shiftedrotated,left_rotated,range_y,range_x)
-            residual_right = self.caldifference_Y_sub(right_shiftedrotated,center_rotated,range_y,range_x)
+            residual_top = self.caldifference_X_sub(center_shifted, top, range_x, range_y)
+            residual_bottom = self.caldifference_X_sub(bottom_shifted, center, range_x, range_y)
 
-            residualy = np.append(np.ravel(residual_left),np.ravel(residual_right))
+            residualx = np.append(np.ravel(residual_top), np.ravel(residual_bottom))
 
-            error = np.append(residualx,residualy)
+            error = np.append(residualx, residualy)
 
             return error
 
-        overlapxy_shiftxy = scipy.optimize.leastsq(errfun, [overlapx0,overlapy0,shiftx0,shifty0])
+        v1xy_v2xy = scipy.optimize.leastsq(errfun, [v1_0[0], v1_0[1], v2_0[0], v2_0[1]])
 
-        return overlapxy_shiftxy
+        return v1xy_v2xy
 
+    def subpixelstitch(self, expandedImages, v1, v2):
 
-    def subpixelstitch(self,expandedImages,overlap_x,overlap_y,shiftx,shifty,rotation):
+        n = np.zeros((expandedImages.shape[1], expandedImages.shape[2]), dtype='float32')
 
-        n = np.zeros((expandedImages.shape[1],expandedImages.shape[2]),dtype='float32')
-        combined = np.zeros((expandedImages.shape[1],expandedImages.shape[2]),dtype='float32')
+        combined = np.zeros((expandedImages.shape[1], expandedImages.shape[2]), dtype='float32')
 
         num_patterns = int(np.sqrt(expandedImages.shape[0]))
 
-        for counter,image in enumerate(expandedImages,start = 0):
+        for counter, image in enumerate(expandedImages, start=0):
+            row = int(num_patterns-1-counter % num_patterns)
+            column = int(counter / num_patterns)
 
-            row = int(num_patterns-1-counter%num_patterns)
-            column = int(counter/num_patterns)
-
-            vector = [overlap_x*column + shiftx*row, overlap_y*row + shifty*column]
-            processed = self.subpixelshift_rotate(image,vector,rotation)
-
+            vector = [v1[0] * column + v2[0] * row, v2[1] * row + v1[1] * column]
+            processed = self.subpixelshift(image, vector)
             combined += processed
             n += processed != 0
 
-
-        combined=combined*(1/n*(n>0))
+        combined = combined*(1/n*(n > 0))*(n > 0)
 
         return combined
-
-    def calculate_residual(self,pattern1,pattern2):
-
-        mask = np.zeros(pattern1.shape,dtype='float32')
-        residual =pattern1 - pattern2
-
-        mask += pattern1 != 0
-        mask += pattern2 != 0
-        mask = mask > 1
-
-        residual = np.divide(residual, pattern1, out=np.zeros_like(residual), where=pattern1!=0)*mask
-
-        return residual
-
-
-
-    def get_residuals(self,expandedImages,overlapx,overlapy,shiftx,shifty,rotation):
-
-        num_patterns = int(np.sqrt(expandedImages.shape[0]))
-        processed_images = np.zeros(expandedImages.shape)
-        residuals = np.zeros(expandedImages[0].shape)
-        n = np.zeros(expandedImages[0].shape,dtype='float32')
-
-        for counter,image in enumerate(expandedImages,start = 0):
-
-            row = int(num_patterns-1-counter%num_patterns)
-            column = int(counter/num_patterns)
-
-            vector = [overlapx*column + shiftx*row, overlapy*row+shifty*column]
-
-            processed = self.subpixelshift_rotate(image,vector,rotation)
-            processed_images[counter,:,:] = processed
-
-
-
-        for location in range(num_patterns*(num_patterns-1)):
-
-            index_left = int(location)
-            index_right = int(location + num_patterns)
-            index_down = int(location/num_patterns + location%num_patterns*num_patterns)
-            index_top = int(index_down + 1)
-
-            residualx = self.calculate_residual(processed_images[index_left],processed_images[index_right])
-            residualy = self.calculate_residual(processed_images[index_down],processed_images[index_top])
-
-            residual = residualx + residualy
-            residuals += residual
-
-            n += residualx != 0
-            n += residualy != 0
-
-        residuals = residuals*(1/n*(n>0))
-
-        return residuals
--- alignment/main_diffalign.py
+++ alignment/main_diffalign.py
@@ -1,42 +1,36 @@
-%matplotlib
-import glob
-import os
-import re
-
 import numpy as np
 from matplotlib import pylab as plt
 from tifffile import imsave
+from alignment import diffalign as dfa
 
-from Processing import diffalign_cleanup as dfa
+#
+filepath = '/Users/xi/Dropbox (MIT)/diffshift_paper/datas/Si_[111]_1o2m_81/Si_111_1o2m_250x250_81shifts_6600avepatterns.tif'
+savepath = '/Users/xi/Dropbox (MIT)/diffshift_paper/datas/Si_[111]_1o2m_81'
 
-plt.rcParams.update({'font.size': 18,'font.weight':'bold','font.family':'Helvetica Neue','axes.linewidth':1.8,'ytick.major.width':2,'ytick.major.size':8,'xtick.major.width':2,'xtick.major.size':8})
-
-#%%
-filepath = '/Volumes/Seagate Backup Plus Drive/Xi_Si_111/Si111_30x80_ser9999_CL2o4_shifts100/averagedStack.tif'
-deterctorsize = [128,128]
-diffalign = dfa.diffalign(filepath,deterctorsize)
+diffalign = dfa.diffalign(filepath)
 
 # display patterns
 allPatterns = diffalign.displaypatterns()
 diffalign.indexPatterns()
 
 # select pattern for alignment
-index = 10
-range_x = 30
-range_y = 40
+index = 40
+range_x = 40
+range_y = 10
 
 # give initial value for alignment
-[overlapx0,overlapy0,shiftx0,shifty0,rotation0] = [45, 45, 0, 0, 0]
+v1_0 = [70, 0]
+v2_0 = [0, 70]
+[[v1_x, v1_y, v2_x, v2_y], success] = diffalign.fit_v1_v2(index, v1_0, v2_0, range_x, range_y)
 
-[[overlapx,overlapy,shiftx,shifty],success] = diffalign.fit_overlapxy_shiftxy(index,overlapx0,overlapy0,shiftx0,shifty0,range_x,range_y)
-
-print([[overlapx, overlapy, shiftx, shifty], success])
+print([[v1_x, v1_y, v2_x, v2_y], success])
 
 # check fitted parameters
 expandedImages = diffalign.expandImages(diffalign.images, 150)
-combined = diffalign.subpixelstitch(expandedImages, overlapx, overlapy, shiftx, shifty, 0)
+combined = diffalign.subpixelstitch(expandedImages, [v1_x, v1_y], [v2_x, v2_y])
 
 plt.figure()
 plt.imshow(np.log(combined))
 
-imsave(savepath+'/combined.tif', combined, shape=combined.shape)
+# imsave(savepath+'/combined.tif', combined, shape=combined.shape)
+
